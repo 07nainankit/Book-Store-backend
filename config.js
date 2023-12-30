a752ac631c4a64bf54adca03e7b18b7c7cd98b1c
07nainankit <98472907+07nainankit@users.noreply.github.com>
2023-12-30 13:44:58 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+//data base 
+export const mongoDBURL = 
+    'mongodb+srv://anain829:eiJS6BLEfULlV5Kj@book-store-mern.a6svc8i.mongodb.net/Books-collection?retryWrites=true&w=majority';
+    
---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import { PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+// till here nothing on localhost:5555 so build routes
+// 404 means we don't have requested url, default route is /
+
+
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+//first http route, CALLback function handle this request
+app.get('/',(request, response) =>{
+    console.log(request);
+    return response.status(234).send('Welcome To BOOKSTORE');
+});
+
+// Middleware for prasing request body
+app.use('/books',booksRoute);
+  
+// connect backend -> we shift get request to then condition 
+// so only when database is connected then only we run server
+mongoose 
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
